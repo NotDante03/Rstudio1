93816f738d1d87d1b1f748f8affb3a3999d4e3b8
NotDante03 <dante.notari@student.univaq.it>
2026-03-10 15:59:49 +0100

    ciao

--- script pull con token.R
+++ script pull con token.R
@@ -4,10 +4,5 @@ system('git config --global user.name "Dante Notari"')
 system("git config --global --list")
 usethis::create_github_token()
 gitcreds::gitcreds_set()
-
-
-
-system('git remote add origin https://github.com/NotDante03/Rstudio1.git')
-system('git remote -v')
-system("git push")
+usethis::use_git_remote(name = "origin" "https://github.com/NotDante03/Rstudio2.git")
 
