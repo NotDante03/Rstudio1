713603a44f147371861b4d423450d1e3d024f95d
NotDante03 <dante.notari@student.univaq.it>
2026-03-12 16:51:40 +0100

    ho finalmente abilitato pull e push

--- 01 init.R
+++ 01 init.R
@@ -4,3 +4,9 @@ system('git config --global user.name "Dante Notari"')
 system("git config --global --list")
 usethis::create_github_token()
 gitcreds::gitcreds_set()
+system('git remote add origin "https://github.com/NotDante03/Rstudio1.git"')
+system("git remote -v") #per vedere se il repository remoto è stato aggiunto correttamente
+system("git remote remove Rstudio1") #per rimuovere un repository remoto sbagliato o ripetuto
+system("git remote remove Rsudio1")
+system("git remote -v")
+system("git push -u origin main") #per pushare i commit su github
