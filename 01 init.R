42a5dcaf793a43094bbc8da66fe393a101aad43d
NotDante03 <dante.notari@student.univaq.it>
2026-03-12 18:06:31 +0100

    token rinnovato perchè mi ero dimenticato di salvarlo da qualche parte

--- 01 init.R
+++ 01 init.R
@@ -3,8 +3,8 @@ system('git config --global user.email "dante.notari@student.univaq.it"')
 system('git config --global user.name "Dante Notari"')
 system("git config --global --list")
 usethis::create_github_token()
-gitcreds::gitcreds_set()
-system('git remote add origin "https://github.com/NotDante03/Rstudio1.git"')
+gitcreds::gitcreds_set() #per salvare il token in modo sicuro sul computer (così non lo reinserisco)
+system('git remote add origin "https://github.com/NotDante03/Rstudio1.git"') #per aggiungere un repository remoto 
 system("git remote -v") #per vedere se il repository remoto è stato aggiunto correttamente
 system("git remote remove Rstudio1") #per rimuovere un repository remoto sbagliato o ripetuto
 system("git remote remove Rsudio1")
